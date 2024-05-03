53152eaaff3d252294f3397460566162ab854da7
lexadev0 <lexadev0@gmail.com>
2024-05-03 13:24:20 +0200

    e2-guardar-informacion

---
+++ js/e2-guardar-informacion.js
@@ -0,0 +1,47 @@
+//1. DECLARAR UNA VARIABLE
+//Nombre de variable recomendado en minuscula
+let nombre;
+//Nombre de variable camelCase
+let miNombre;
+//Nombre de variable separada con guiones para los espacios
+let mi_nombre;
+//los nombres de las variables no pueden comenzar con un numero 2Nombre, pero pueden tener numero al final
+let nombre2;
+
+//2. INICIAR UNA VARIABLE
+//Primero declarar la variable y luego iniciarza
+let ciudad;
+ciudad = 'Zaragoza';
+document.write('Ejemplo 1. Iniciar la variable ciudad <br>');
+document.write('La variable ciudad tiene valor ' + ciudad);
+//Declarar variable e iniciarla a la vez
+let edad = 31;
+document.write('<br>Ejemplo 2. Iniciar la variable edad <br>');
+document.write('La variable edad tiene valor ' + edad);
+
+//TIPOS DE VARIABLES
+//NUMERO
+let numero = 5;
+let numeroDecimal = 5.5;
+document.write('<br>Ejemplo 3. Iniciar la variable tipo numero <br>');
+document.write('El numero sin decimales es ' + numero);
+document.write('<br>');
+document.write('El numero con decimales es ' + numeroDecimal);
+
+//CADENAS. Pueden usarse comillas simples o dobles
+let saludo = 'Hola que tal?';
+let adios = 'Adios, me voy';
+document.write('<br>Ejemplo 4. Iniciar la variable tipo texto - string <br>');
+document.write('La variable de texto saludo es: ' + saludo);
+document.write('<br>');
+document.write('La variable de texto adios es: ' + adios);
+
+//BOOLEANOS. True o False
+let estaSoleado = true;
+let esDeNoche = false;
+document.write('<br>Ejemplo 5. Iniciar la variable tipo boolean <br>');
+document.write('La variable esta soleado es: ' + estaSoleado);
+document.write('<br>')
+document.write('La variable esta de noche es: ' + esDeNoche);
+
+
