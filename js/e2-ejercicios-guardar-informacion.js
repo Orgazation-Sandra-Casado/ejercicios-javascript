1cb87c5bfeadf1cf21d62c70b7726a678f0a2bd1
lexadev0 <lexadev0@gmail.com>
2024-05-06 11:49:50 +0200

    ejercicios y ejemplos separados

---
+++ js/e2-ejercicios-guardar-informacion.js
@@ -0,0 +1,72 @@
+document.write('<hr>')
+document.write('<br>EJERCICIOS<br>')
+
+//DECLARACION DE VARIABLES PARA EL EJERCICIO 1
+nombre = 'Sandra';
+let apellidos = 'Casado Aznar';
+let altura = 1.65;
+let playa = true;
+let montaña = true;
+let chocolate = true;
+let mascota = true;
+
+document.write('Mi nombre es: ' + nombre);
+document.write('<br> Mis apellidos son: ' + apellidos);
+document.write('<br> Mi edad es: ' + edad);
+document.write('<br> Mi altura es: ' + altura);
+document.write('<br> Mi ciudad es: ' + ciudad);
+document.write('<br> Me gusta la playa: ' + playa);
+document.write('<br> Me gusta la montaña: ' + montaña);
+document.write('<br> Me gusta el chocolate: ' + chocolate);
+document.write('<br> Tengo mascota: ' + mascota);
+
+document.write('<br>')
+document.write('<br>')
+
+//DECLARACION DE VARIABLES PARA EL EJERCICIO 2
+let tituloPelicula = 'Inglorious Basterds';
+let directorPelicula = 'Tarantino';
+let añoEstreno = 2009;
+let generoPelicula = 'Belico';
+let duracionPelicula = 153;
+let premiosPelicula = true;
+
+document.write('Titulo pelicula: ' + tituloPelicula);
+document.write('<br> Director de la pelicula: ' + directorPelicula);
+document.write('<br> Año de estreno: ' + añoEstreno);
+document.write('<br> Genero de la pelicula: ' + generoPelicula);
+document.write('<br> Duracion: ' + duracionPelicula);
+document.write('<br> Tiene premios: ' + premiosPelicula);
+document.write('<br>');
+
+tituloPelicula = 'Interstellar';
+directorPelicula = 'Christopher Nolan';
+añoEstreno = 2014;
+generoPelicula = 'Ficcion';
+duracionPelicula = 169;
+premiosPelicula = true;
+
+document.write('Titulo pelicula: ' + tituloPelicula);
+document.write('<br> Director de la pelicula: ' + directorPelicula);
+document.write('<br> Año de estreno: ' + añoEstreno);
+document.write('<br> Genero de la pelicula: ' + generoPelicula);
+document.write('<br> Duracion: ' + duracionPelicula);
+document.write('<br> Tiene premios: ' + premiosPelicula);
+
+document.write('<br>')
+document.write('<br>')
+
+//DECLARACION DE VARIABLES PARA EL EJERCICIO 3
+let tituloCancion = 'Anoana';
+let artista = 'Heilung';
+let album = 'Drif';
+let añoLanzamiento = 2022;
+let duracionCancion = 4.57;
+let videoclip = true;
+
+document.write('Titulo cancion: ' + tituloCancion);
+document.write('<br> Artista: ' + artista);
+document.write('<br> Album ' + album);
+document.write('<br> Año de lanzamiento: ' + añoLanzamiento);
+document.write('<br> Duracion: ' + duracionCancion);
+document.write('<br> Tiene videoclip: ' + videoclip);
