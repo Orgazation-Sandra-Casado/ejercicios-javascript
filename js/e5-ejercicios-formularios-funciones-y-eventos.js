1225ee546778aabacc552dd96c2f56fcca25eb96
lexadev0 <lexadev0@gmail.com>
2024-05-14 20:43:07 +0200

    e5-ejercicio1

---
+++ js/e5-ejercicios-formularios-funciones-y-eventos.js
@@ -0,0 +1,8 @@
+//Ejercicio 1: Nombre, ciudad y año de nacimiento
+function wellcome() {
+    let nombre = document.getElementById("ej1Nombre").value;
+    let ciudad = document.getElementById("ej1Ciudad").value;
+    let fechaNacimiento = document.getElementById("ej1Fecha").value;
+    let mensaje = "Hola " + nombre + " bienvenida!" + " Vive en " + ciudad + " y ha nacido en " + fechaNacimiento;
+    imprimir(mensaje, "respuesta1")
+}
