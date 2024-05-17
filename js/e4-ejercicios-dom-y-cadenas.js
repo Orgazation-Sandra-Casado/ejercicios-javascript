9b43d411aca85bb2c278a24d77b3ef918d3c4245
lexadev0 <lexadev0@gmail.com>
2024-05-17 10:18:47 +0200

    e4-ejercicios-cadenas-2

--- js/e4-ejercicios-dom-y-cadenas.js
+++ js/e4-ejercicios-dom-y-cadenas.js
@@ -43,3 +43,52 @@ parrafo.innerText = 'Cambio de nombre ' + welcomeMessage;
 
 parrafo = document.getElementById('e1fraseSinQueTal');
 parrafo.innerText = 'Frase sin Que Tal ' + mensajeBienvenidaSinQueTal;
+
+/*Ejercicio 2
+Crea una variable con una frase en la que te presentes y muestrala junto con:
+Su longitud
+La posición de tu nombre
+La primera letra de la frase.
+La última letra de la frase.
+Muestra la frase completa en mayúscula
+Muestra la frase completa en minúscula
+Muestra la frase cambiando tu nombre por otro que elijas.
+Crea una nueva palabra, con la misma frase quitando tu nombre.*/
+
+let fraseEj2 = 'Hola, me llamo Sandra y me gusta la fotografia';
+let longitudEj2 = fraseEj2.length;
+let posicionNombreEj2 = fraseEj2.indexOf('Sandra');
+let primeraLetraEj2 = fraseEj2[0];
+let ultimaLetraEj2 = fraseEj2[longitudEj2 - 1];
+let frase2Mayuscula = fraseEj2.toUpperCase();
+let frase2Minuscula = fraseEj2.toLowerCase();
+let segundoNombre = 'Ramses';
+let cambioNombre = fraseEj2.replace('Sandra', segundoNombre);
+let sinNombre = fraseEj2.replace(' me llamo Sandra', '');
+
+let cadenas = document.getElementById('e2longitud');
+cadenas.innerText = 'La longitud de la frase ' + fraseEj2 + ' es ' + longitudEj2;
+
+cadenas = document.getElementById('e2posicion');
+cadenas.innerText = 'La posicion del nombre es ' + posicionNombreEj2;
+
+cadenas = document.getElementById('e2primera');
+cadenas.innerText = 'La primera letra es ' + primeraLetraEj2;
+
+cadenas = document.getElementById('e2ultima');
+cadenas.innerText = 'La ultima letra es ' + ultimaLetraEj2;
+
+cadenas = document.getElementById('e2mayuscula');
+cadenas.innerText = 'La frase en mayusculas es ' + frase2Mayuscula;
+
+cadenas = document.getElementById('e2minuscula');
+cadenas.innerText = 'La frase en minusculas es ' + frase2Minuscula;
+
+cadenas = document.getElementById('e2segundoNombre');
+cadenas.innerText = 'La frase con nuevo nombre es ' + cambioNombre; 
+
+cadenas = document.getElementById('e2sinNombre');
+cadenas.innerText = 'La frase sin nombre es ' + sinNombre;
+
+
+
