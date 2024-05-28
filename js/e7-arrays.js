871dc56884ffbbab8665ec6543339da627abd384
lexadev0 <lexadev0@gmail.com>
2024-05-28 13:30:36 +0200

    e7-array-modificar

--- js/e7-arrays.js
+++ js/e7-arrays.js
@@ -1,3 +1,6 @@
+//Variables Globales. Estan fuera de la funcion y se pueden usar en cualquier funcion que este en el mismo fichero js
+let listaCompra = ['tomates', 'huevos', 'queso', 'fresas', 'zanahorias','atun'];
+
 function imprimir(mensaje, id) {
     let elemento = document.getElementById(id);
     elemento.innerText = mensaje;
@@ -6,18 +9,23 @@ function imprimir(mensaje, id) {
 //Ejemplo 1. Crear array y mostrarlo
 //Puede tener tanto texto como números
 function mostrarLista() {
-    let listaCompra = ['tomates', 'huevos', 'queso', 'fresas', 'zanahorias'];
     mensaje = 'La lista de la compra es ' + listaCompra;
     imprimir(mensaje, 'e1listaresultado');
 }
 
 //Ejemplo 2. Mostrar el elemento de la posicion que se elija de la lista
 function mostrarInfoLista() {
-    let listaCompra = ['tomates', 'huevos', 'queso', 'fresas', 'zanahorias'];
     let primerProducto = listaCompra[0];
     let longitudLista = listaCompra.length;
     let ultimoProducto = listaCompra[longitudLista - 1];
-    let mensaje = 'La lista de la compra es ' + listaCompra + ' el primer elemento es ' + primerProducto
-        + ' el ultimo es ' + ultimoProducto + ' y la longitud de la lista es ' + longitudLista;
+    let mensaje = 'El primer elemento es ' + primerProducto + ' el ultimo es ' + ultimoProducto + ' y la longitud de la lista es ' + longitudLista;
     imprimir(mensaje, 'e2listainfo');
+}
+
+//Ejemplo 3. Modificar array
+function modificarLista() {
+    let posicionModificar = parseInt(document.getElementById('e3posicion').value);
+    let nuevoValor =document.getElementById('e3nuevovalor').value;
+    listaCompra[posicionModificar-1] = nuevoValor;
+    mostrarLista();
 }
