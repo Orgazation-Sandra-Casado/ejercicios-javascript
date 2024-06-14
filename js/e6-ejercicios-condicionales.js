b637f40e0a25eb7177f47bd1d6eccf743cd9738c
lexadev0 <lexadev0@gmail.com>
2024-06-14 12:58:42 +0200

    e6-ejercicios-condicionales

--- js/e6-ejercicios-condicionales.js
+++ js/e6-ejercicios-condicionales.js
@@ -34,4 +34,50 @@ function cualMayor() {
     imprimir(mensaje, 'ej2Mayor');
 }
 
+//Ejercicio 3: Formulario para ver si el alumno esta aprobado o no
+function notaExamen(){
+    let nota = parseFloat(document.getElementById('ej3nota').value);
+    let mensaje = '';
+
+    if(nota >= 5){
+        mensaje ='APROBADO';
+    }else{
+        mensaje ='SUSPENDIDO';
+    }
+    imprimir(mensaje,'ejer3resultado');
+}
+
+//Ejercicio 4: Comprobar longitud de un nombre si es largo o corto
+function nombreCortoLargo(){
+    let nombre = document.getElementById('ejercicio4nombre').value;
+    let mensaje = '';
+
+    if(nombre.length >= 10){
+        mensaje = 'Largo';
+    }else{
+        mensaje = 'Corto';
+    }
+    imprimir(mensaje,'ejercicio4resultado')
+}
+
+//Ejercicio 5: Ver la estacion del año
+function mesEstacion(){
+    let mes = document.getElementById("ex5").value;
+    let mensaje = "";
+    if (mes === "Diciembre" || mes === "Febrero" || mes === "Enero") {
+        mensaje = "El mes seleccionado es " + mes + " por tanto estamos en Invierno";
+    }
+    else if (mes === "Marzo" || mes === "Abril" || mes === "Mayo") {
+        mensaje = "El mes seleccionado es " + mes + " por tanto estamos en Primavera";
+    }
+    else if (mes === "Junio" || mes === "Julio" || mes === "Agosto") {
+        mensaje = "El mes seleccionado es " + mes + " por tanto estamos en Verano";
+    }
+    else if (mes === "Septiembre" || mes === "Octubre" || mes === "Noviembre") {
+        mensaje = "El mes seleccionado es " + mes + " por tanto estamos en Otoño";
+    }
+    imprimir(mensaje, "ex5resultado");
+}
+
+
 
