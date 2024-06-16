0a3f4333b76a0212a908ae1cc30a5fd874ae2d9d
lexadev0 <lexadev0@gmail.com>
2024-06-16 18:31:47 +0200

    e6-ejercicios

--- js/e6-ejercicios-condicionales.js
+++ js/e6-ejercicios-condicionales.js
@@ -35,33 +35,33 @@ function cualMayor() {
 }
 
 //Ejercicio 3: Formulario para ver si el alumno esta aprobado o no
-function notaExamen(){
+function notaExamen() {
     let nota = parseFloat(document.getElementById('ej3nota').value);
     let mensaje = '';
 
-    if(nota >= 5){
-        mensaje ='APROBADO';
-    }else{
-        mensaje ='SUSPENDIDO';
+    if (nota >= 5) {
+        mensaje = 'APROBADO';
+    } else {
+        mensaje = 'SUSPENDIDO';
     }
-    imprimir(mensaje,'ejer3resultado');
+    imprimir(mensaje, 'ejer3resultado');
 }
 
 //Ejercicio 4: Comprobar longitud de un nombre si es largo o corto
-function nombreCortoLargo(){
+function nombreCortoLargo() {
     let nombre = document.getElementById('ejercicio4nombre').value;
     let mensaje = '';
 
-    if(nombre.length >= 10){
+    if (nombre.length >= 10) {
         mensaje = 'Largo';
-    }else{
+    } else {
         mensaje = 'Corto';
     }
-    imprimir(mensaje,'ejercicio4resultado')
+    imprimir(mensaje, 'ejercicio4resultado')
 }
 
 //Ejercicio 5: Ver la estacion del año
-function mesEstacion(){
+function mesEstacion() {
     let mes = document.getElementById("ex5").value;
     let mensaje = "";
     if (mes === "Diciembre" || mes === "Febrero" || mes === "Enero") {
@@ -79,5 +79,122 @@ function mesEstacion(){
     imprimir(mensaje, "ex5resultado");
 }
 
+//Ejercicio 6: Calculadora
+function calculadoraNumeros() {
+    let numero1 = parseFloat(document.getElementById("e6calculadora").value);
+    let numero2 = parseFloat(document.getElementById("e6calculadora2").value);
+    let operacion = document.getElementById("e6operacion").value;
+    let resultado = 0;
+    let mensaje = "";
+
+    if (operacion === "sumar") {
+        resultado = numero1 + numero2;
+        mensaje = "La suma de " + numero1 + " mas " + numero2 + " es igual a: " + resultado;
+    }
+    else if (operacion === "restar") {
+        resultado = numero1 - numero2;
+        mensaje = "La resta de " + numero1 + " menos " + numero2 + " es igual a: " + resultado;
+    }
+    else if (operacion === "multiplicar") {
+        resultado = numero1 * numero2;
+        mensaje = "La multiplicación de " + numero1 + " por " + numero2 + " es igual a: " + resultado;
+    }
+    else if (operacion === "dividir") {
+        if (numero2 !== 0) {
+            resultado = numero1 / numero2;
+            mensaje = "La división de " + numero1 + " entre " + numero2 + " es igual a: " + resultado;
+        }
+        else {
+            mensaje = "No se puede dividir entre 0";
+        }
+    }
+    else {
+        mensaje = "Operacion no permitida";
+    }
 
 
+    imprimir(mensaje, "e6calculadoraresultado");
+}
+
+// Mismo ejemplo con SWITCH
+//VERSION EJERCICIO 6 CON SWITCH
+function calculadoraNumerosSwitch() {
+    let numero1 = parseFloat(document.getElementById("e6calculadora").value);
+    let numero2 = parseFloat(document.getElementById("e6calculadora2").value);
+    let operacion = document.getElementById("e6operacion").value;
+    let resultado = 0;
+    let mensaje = "";
+
+    switch (operacion) {
+        case "sumar":
+            resultado = numero1 + numero2;
+            mensaje = mensaje = "La suma de " + numero1 + " más " + numero2 + " es igual a: " + resultado;
+            break;
+        case "restar":
+            resultado = numero1 - numero2;
+            mensaje = "La resta de " + numero1 + " menos " + numero2 + " es igual a: " + resultado;
+            break;
+        case "multiplicar":
+            resultado = numero1 * numero2;
+            mensaje = "La multiplicación de " + numero1 + " por " + numero2 + " es igual a: " + resultado;
+            break;
+        case "dividir":
+            if (numero2 !== 0) {
+                resultado = numero1 / numero2;
+                mensaje = "La división de " + numero1 + " entre " + numero2 + " es igual a: " + resultado;
+            }
+            else {
+                mensaje = "No se puede dividir entre 0";
+            }
+            break;
+        default:
+            mensaje = "Operacion no permitida";
+    }
+    imprimir(mensaje, "e6calculadoraresultado");
+
+}
+
+//Ejercicio 7
+function calcularPrecioFinal() {
+    let producto = document.getElementById('producto').value;
+    let cantidad = parseInt(document.getElementById('cantidad').value);
+    let precio = 0;
+    let mensaje = '';
+    let precioProducto = 0;
+
+    if (producto === 'camiseta') {
+        precioProducto = 10;
+    } else if (producto === 'sudadera') {
+        precioProducto = 20;
+    } else if (producto === 'bufanda') {
+        precioProducto = 5;
+    }
+    precio = precioProducto * cantidad;
+    mensaje = 'El precio del producto es ' + precio;
+    imprimir(mensaje, 'ex7Resultado');
+}
+
+//Ejercicio 8
+function cuantasNoches() {
+    let habitacion = document.getElementById('ejer8habitacion').value;
+    let numeroNoches = parseInt(document.getElementById('ejer8cantidadNoches').value);
+    let precioNoche = 0;
+    let precioFinal = 0;
+    let mensaje = '';
+
+    if (habitacion === 'suite') {
+        precioNoche = 100;
+    } else if (habitacion === 'doble') {
+        precioNoche = 70;
+    } else if (habitacion === 'individual') {
+        precioNoche = 50;
+    }
+
+    precioFinal = precioNoche * numeroNoches;
+
+    if (numeroNoches > 5) {
+        precioFinal = precioFinal * 0.8;
+    }
+    mensaje = 'El precio final es ' + precioFinal;
+    imprimir(mensaje, 'ejer8resultadonoches');
+}
